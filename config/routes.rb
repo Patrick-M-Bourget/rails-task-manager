357546497b9072e91e3116fba9f7492fa2eb3498
Patrick Bourget <patrickmbourget@gmail.com>
2020-01-25 15:26:16 -0500

    new task working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Display all tasks
+  get "tasks", to: "tasks#index"
+
+  # Add a new post
+  get "tasks/new", to: "tasks#new"
+  post "tasks", to: "tasks#create"
+
+  # Display one task
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Update a task
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  # Delete a task
+  delete "tasks/:id", to: "tasks#destroy"
 end
