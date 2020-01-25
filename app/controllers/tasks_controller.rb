9eedf02d9c7a7b91c97dce471cf82f3ffd014121
Patrick Bourget <patrickmbourget@gmail.com>
2020-01-25 16:30:47 -0500

    all working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,9 +6,7 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -21,19 +19,15 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
-    @task.update(restaurant_params)
+    @task.update(task_params)
 
     redirect_to tasks_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     redirect_to tasks_path
